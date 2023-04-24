bbde5e66bfe74247e83a7ceecfb92909925b5377
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-04-24 15:50:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function threeSum(dec) {
-// write your code here
-	let bin = '';
-    while(dec>0){
-        let rem = dec%2;
-        bin = rem + bin;
-        dec = Math.floor(dec/2);
-    }
-    return bin;
-  
+function decimalToBinary(dec) {
+  let bin = '';
+  while (dec > 0) {
+    let rem = dec % 2;
+    bin = rem + bin;
+    dec = Math.floor(dec / 2);
+  }
+  return bin;
 }
-
-module.exports = threeSum;
